ae997f8ebb11cace78c3c6a3f98e97fcd930bea7
Viktor Matuizo <99718345+Vitek169@users.noreply.github.com>
2025-05-15 17:25:22 +0300

    add file: - playwright_disabled.py. - playwright_keyboard.py. - playwright_js.py

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright, expect
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
-    page.goto(' https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
 
     email_input = page.get_by_test_id('login-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
@@ -18,3 +18,7 @@ with sync_playwright() as playwright:
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
     page.wait_for_timeout(5000)
+
+
+
+

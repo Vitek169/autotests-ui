80902b59eb8e1e5ceea952f542a5d3573bc0566e
Viktor Matuizo <99718345+Vitek169@users.noreply.github.com>
2025-05-27 19:55:21 +0300

    Добавил print-ы в файл playwright_registration_button.py

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -12,6 +12,7 @@ with sync_playwright() as playwright:
     # Через блок try-except проверяем что кнопка Registration выключена, если она включена, выйдет соответствующее сообщение
     try:
         expect(registration_button).to_be_disabled()
+        print('Button is Disabled')
     except AssertionError:
         print('Кнопка должна быть не видна')
 
@@ -30,6 +31,7 @@ with sync_playwright() as playwright:
     # Через блок try-except проверяем что кнопка Registration включена, если она выключена, выйдет соответствующее сообщение
     try:
         expect(registration_button).to_be_enabled()
+        print('Button is Enabled')
     except AssertionError:
         print('Кнопка должна быть видна')
 
